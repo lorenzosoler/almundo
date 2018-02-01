f52dbb13feb7be9385a87686b08d69788d0d294a
lorenzo.soler <lorenzo@ntbflux-Lsoler>
2018-02-01 16:25:00 -0300

    avanze

--- src/services/hoteles.service.ts
+++ src/services/hoteles.service.ts
@@ -1,9 +1,31 @@
+import {environment} from '../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/map';
+import { Hotel } from "../models/hotel";
 
 @Injectable()
 export class HotelesService {
+  private endpoint = environment.endpoint;
 
   constructor(public http: HttpClient) { }
+
+  public getAllHoteles (): Observable<Hotel[]> {
+    return this.http.get(this.endpoint + '/hoteles').map(this.mapHoteles);
+  }
+
+  private mapHoteles(hoteles: Array<any>) {
+    let result: Array<Hotel> = [];
+
+    if (hoteles) {
+      hoteles.forEach((hotel: any) => {
+        result.push(
+          new Hotel(hotel)
+        )
+      });
+    }
+    return result;
+  }
 
 }
